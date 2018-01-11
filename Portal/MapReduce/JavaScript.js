f9e8871043e2f764ad1c2bf90b16aa451522c446
Ivan Grgurina <ivan.grgurina@gmail.com>
2018-01-11 04:57:35 +0100

    fixed small typo, cleaned article.cs

--- Portal/MapReduce/JavaScript.js
+++ Portal/MapReduce/JavaScript.js
@@ -16,37 +16,39 @@
                 emit(_id, value); // store a  for each word
             }
         }
-    };
+    }
+};
 
-    var reduce = function (key, values) {
-        return Array.sum(values);
-    };
+var reduce = function (key, values) {
+    return Array.sum(values);
+};
 
-    db.articles.mapReduce(map, reduce, { limit: 1000, out: "word_count" });
-    db.word_count.find().sort({ value: -1 }).limit(10);
+db.articles.mapReduce(map, reduce, { limit: 1000, out: "word_count" });
+db.word_count.find().sort({ value: -1 }).limit(10);
 
 
-    // word_count SCHEMA:
-    //_id = {author: <string>, word: <string>}, value: <int>
-    var mapper = function () {
-        emit(this._id.author, { top: [{ word: this._id.word, total: this.value }] });
-    };
+// word_count SCHEMA:
+//_id = {author: <string>, word: <string>}, value: <int>
+var mapper = function () {
+    emit(this._id.author, { top: [{ word: this._id.word, total: this.value }] });
+};
 
-    var reducer = function (key, values) {
-        //var scores = [];
-        var result = [];// { word: " ", scores: [] };
-        values.forEach(
-            function (value) { // value = { top [{ word, total }] }
-                value.top.forEach(
-                    function (score) { // score = {word, total}
-                        result[result.length] = { word: score.word, total: score.total }; //  scores[scores.length] 
-                    });
-            });
-        //var descending =
-        result.sort((a, b) => Number(b.total) - Number(a.total));
-        //scores.sort();
-        //scores.reverse();
-        return { top: result.slice(0, 10) };
-    };
+var reducer = function (key, values) {
+    //var scores = [];
+    var result = [];// { word: " ", scores: [] };
+    values.forEach(
+        function (value) { // value = { top [{ word, total }] }
+            value.top.forEach(
+                function (score) { // score = {word, total}
+                    result[result.length] = { word: score.word, total: score.total }; //  scores[scores.length] 
+                });
+        });
+    //var descending =
+    result.sort((a, b) => Number(b.total) - Number(a.total));
+    //scores.sort();
+    //scores.reverse();
+    return { top: result.slice(0, 10) };
+};
 
-    db.word_count.mapReduce(mapper, reducer, "top_author_words");
+db.word_count.mapReduce(mapper, reducer, "top_author_words");
+
